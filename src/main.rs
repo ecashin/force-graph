c4aee05be4d08d6bc25d2427c7f682f254aaf145
Ed Cashin <ed.cashin@acm.org>
2021-08-01 15:21:33 -0400

    Transition to web app

--- src/main.rs
+++ src/main.rs
@@ -1,158 +1,73 @@
-use clap::clap_app;
-use ndarray::{Array1, Array2, ArrayView1, Axis};
-use rand::prelude::*;
-use std::collections::{HashMap, HashSet};
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
 
-#[derive(Debug)]
-struct Edge {
-    src: usize,
-    dst: usize,
+mod glayout;
+
+struct GraphConfig {
+    n_vertices: usize,
+    n_dimensions: usize,
+    max_degree: usize,
 }
 
-fn initial_positions(n: usize, n_dims: usize) -> Array2<f32> {
-    let mut a = Array2::zeros((n, n_dims));
-    let mut rng = thread_rng();
+struct LayoutConfig {
+    n_iters: usize,
+}
 
-    for i in 0..n {
-        for j in 0..n_dims {
-            a[[i, j]] = rng.gen_range(0.0..=1.0);
+enum Msg {
+    NewGraph(GraphConfig),
+    Layout(LayoutConfig),
+}
+
+struct Model {
+    link: ComponentLink<Self>,
+    vertices: Vec<Vec<f32>>,
+    edges: Vec<usize>,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            vertices: Vec::new(),
+            edges: Vec::new(),
         }
     }
-    a
-}
 
-fn add_edges(n_vertices: usize, max_degree: usize) -> Vec<Edge> {
-    let mut rng = thread_rng();
-    let mut degrees = vec![0; n_vertices];
-    let mut edges: HashSet<(usize, usize)> = HashSet::new();
-    for src in 0..n_vertices {
-        let degree = rng.gen_range(1..max_degree);
-        let mut n_attempts = 3;
-        while degrees[src] < degree && n_attempts > 0 {
-            let dst = rng.gen_range(0..n_vertices);
-            if src == dst {
-                continue;
-            }
-            if dst < src {
-                if degrees[dst] >= max_degree {
-                    continue;
-                }
-                edges.insert((dst, src));
-            } else {
-                edges.insert((src, dst));
-            }
-            degrees[src] += 1;
-            degrees[dst] += 1;
-            n_attempts -= 1;
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::NewGraph(GraphConfig {
+                n_vertices,
+                n_dimensions,
+                max_degree,
+            }) => true,
+            Msg::Layout(LayoutConfig { n_iters }) => true,
         }
     }
-    edges
-        .into_iter()
-        .map(|(src, dst)| Edge { src, dst })
-        .collect()
-}
 
-fn print_js(pos: Array2<f32>, edges: Vec<Edge>) {
-    println!("(function () {{");
-    println!("    var g = G.graph()");
-    println!("    var nodes = []");
-    println!("    var edge, node");
-    for row in pos.rows() {
-        println!("    node = G.node({}, {{\"color\": \"black\"}})", row);
-        println!("    nodes.push(node); node.addTo(g)");
-    }
-    for Edge { src, dst } in edges.into_iter() {
-        println!(
-            "    edge = G.edge([nodes[{}], nodes[{}]], {{color: \"blue\"}})",
-            src, dst
-        );
-        println!("    edge.addTo(g)");
-    }
-    println!("    g.renderIn(\"graph\")");
-    println!("}})()\n");
-}
-
-fn dist(x1: &ArrayView1<f32>, x2: &ArrayView1<f32>) -> f32 {
-    let diff = x1 - x2;
-
-    (&diff * &diff).sum().sqrt()
-}
-
-fn force_graph(pos: &mut Array2<f32>, edges: &[Edge]) {
-    const N_ITERS: usize = 100;
-    let ideal_dist: f32 = 1.0 / pos.nrows() as f32;
-    let mut edges_by_node: HashMap<usize, Vec<usize>> = HashMap::new();
-    const CTR_WEIGHT: f32 = 0.1;
-    let spread_weight: f32 = 0.0 / pos.len_of(Axis(0)) as f32;
-    let repel_weight: f32 = 0.0 / pos.len_of(Axis(0)) as f32;
-
-    for Edge { src, dst } in edges {
-        let mut v = edges_by_node.entry(*src).or_insert_with(Vec::new);
-        v.push(*dst);
-        v = edges_by_node.entry(*dst).or_insert_with(Vec::new);
-        v.push(*src);
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
     }
 
-    for iter_no in 0..N_ITERS {
-        let mut new_positions = pos.to_owned();
-        for (i, row) in pos.outer_iter().enumerate() {
-            println!("{} row:{} orig_row:{:?}", iter_no, i, row);
-            let mut repel: Array1<f32> = Array1::zeros(row.len());
-            for other in pos.rows() {
-                if row == other {
-                    continue;
-                }
-                repel = repel + repel_weight * (&row - &other) * dist(&row, &other);
-            }
-            println!("{} row:{} repel:{:?}", iter_no, i, repel);
-            let ctr = (&row * &row).sum().sqrt() * (-1.0 * &row) * CTR_WEIGHT;
-            println!("{} row:{} ctr:{:?}", iter_no, i, ctr);
-            let mut spread: Array1<f32> = Array1::zeros(row.len());
-            for (point, neighbors) in &edges_by_node {
-                let point = &pos.index_axis(Axis(0), *point);
-                for i in neighbors {
-                    let neighbor = &pos.index_axis(Axis(0), *i);
-                    let obs_dist = dist(&point, &neighbor);
-                    let direction = (neighbor - point) / obs_dist;
-                    spread = spread + spread_weight * (obs_dist - ideal_dist) * direction;
-                }
-            }
-            println!("{} row:{} spread:{:?}", iter_no, i, spread);
-            let mut new_row = new_positions.index_axis_mut(Axis(0), i);
-            new_row += &(repel + ctr + spread);
-            println!("{} row:{} new_row:{:?}", iter_no, i, new_row);
+    fn view(&self) -> Html {
+        html! {
+            <div>
+
+            </div>
         }
-        pos.assign(&new_positions);
     }
 }
 
 fn main() {
-    let matches = clap_app!(forcegraph =>
-        (author: "Ed.Cashin@acm.org")
-        (about: "trivial force-directed graph arrangement")
-        (@arg N: -n +takes_value "Number of vertices")
-        (@arg MAX_DEGREE: -m +takes_value "Max degree")
-        (@arg DIMS: -d +takes_value "Number of dimensions")
-    )
-    .get_matches();
-
-    macro_rules! get_arg {
-        ($argname:expr, $default:expr) => {
-            match matches.value_of($argname) {
-                None => $default,
-                Some(n) => n.parse::<usize>().unwrap(),
-            };
-        };
-    }
-    let n = get_arg!("N", 10);
-    let d = get_arg!("DIMS", 3);
-    let m = get_arg!("MAX_DEGREE", 2);
-
-    let mut pos = initial_positions(n, d);
-    let edges = add_edges(n, m);
-    force_graph(&mut pos, &edges);
+    /*
+    let mut pos = crate::glayout::initial_positions(n, d);
+    let edges = crate::glayout::add_edges(n, m);
+    crate::glayout::force_graph(&mut pos, &edges);
 
     eprintln!("n:{} m:{} d:{}", n, m, d);
 
-    print_js(pos, edges);
+    crate::glayout::print_js(pos, edges);
+    */
+    yew::start_app::<Model>()
 }
